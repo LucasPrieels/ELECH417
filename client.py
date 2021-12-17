0941bc08e214386bd00e107d1a8ec46aab303ced
Lucas Prieels <lucas@MacBook-Pro-de-Lucas-2.local>
2021-12-18 00:40:07 +0100

    Possibility to send messages from any client to the server, multiple clients connection supported

--- client.py
+++ client.py
@@ -1,33 +1,35 @@
 import socket, time
 
-def connect_to_server(ip):
+def connect_to_server(ip, remote_port):
     s = socket.socket()    # Remote socket
     host = socket.gethostname() # Remote address (here same as local since local tests)
-    port = 9999 # Remote port number
     if ip == "-1": # If local
         ip = host
-    s.connect((ip,port))
+    print("Remote port : " + str(remote_port))
+    s.connect((ip, remote_port))
     return s
 
 def login(ip):
     print("####### LOGIN #######")
     usr = input("Username : ")
     pswd = input("Password : ")
-    server_socket = connect_to_server(ip)
+    server_main_socket = connect_to_server(ip, 10000) # The server port number to connect is 10000
 
-    server_socket.send(str.encode("0")) # Code stating we want to login
-    server_socket.send(str.encode(str(usr) + " " + str(pswd))) # str.encode() to transform the string into bytes
+    server_main_socket.send(str.encode("0")) # Code stating we want to login
+    server_main_socket.send(str.encode(str(usr) + " " + str(pswd))) # str.encode() to transform the string into bytes
     
-    ans = bytes.decode(server_socket.recv(1)) # 1 byte is enough
+    ans = bytes.decode(server_main_socket.recv(1)) # 1 byte is enough
     if ans == "0":
         print("Username unkown\n")
-        return -1
+        return -1, -1, -1
     elif ans == "1":
         print("Password incorrect\n")
-        return -1
+        return -1, -1, -1
     elif ans == "2":
         print("Connection successful\n")
-        return usr
+        remote_port = bytes.decode(server_main_socket.recv(4)) # 4 bytes for a port number between 2000 and 3000 (in string format so each character takes a byte)
+        listen_socket = connect_to_server(ip, int(remote_port)) # Connect to the new port specific for this client, given by the server
+        return usr, server_main_socket, listen_socket
     else:
         raise Exception("Unexpected answer")
         
@@ -35,12 +37,12 @@ def signup(ip):
     print("####### SIGNUP #######")
     usr = input("Username : ")
     pswd = input("Password : ")
-    server_socket = connect_to_server(ip)
+    server_main_socket = connect_to_server(ip, 10000) # The server port number to connect is 10000
     
-    server_socket.send(str.encode("1")) # Code stating we want to sign up
-    server_socket.send(str.encode(str(usr) + " " + str(pswd))) # str.encode() to transform the string into bytes
+    server_main_socket.send(str.encode("1")) # Code stating we want to sign up
+    server_main_socket.send(str.encode(str(usr) + " " + str(pswd))) # str.encode() to transform the string into bytes
     
-    ans = bytes.decode(server_socket.recv(1)) # 1 byte is enough, it's the status of the query
+    ans = bytes.decode(server_main_socket.recv(1)) # 1 byte is enough, it's the status of the query
     if ans == "0":
         print("Username already used\n")
         return -1
@@ -57,6 +59,13 @@ while True:
     if log == "0":
         signup(ip)
     elif log == "1":
-        login(ip)
+        usr, server_main_socket, listen_socket = login(ip)
+        if usr != -1:
+            print("You can now send messages, hit return to send them and send '.' to stop")
+            while True:
+                data = input()
+                server_main_socket.send(str.encode(data))
+                if data == ".": # Stop communication
+                    break
 #finally:
-    #server_socket.close()
+    #server_main_socket.close()
--- server.py
+++ server.py
@@ -1,17 +1,19 @@
-import socket
+import socket, random
+from _thread import *
 
 clients = {} # List of clients connected with their username and their socket
+current_port = 0
 
-def parse_credentials_file():
+def parse_credentials_file(): # Read the credentials file to get the users and passwords
     credentials = {}
     f = open("credentials.txt", "r")
     for line in f:
         usr, pswd = (line.strip()).split(' ')
         credentials[usr] = pswd
-    print(credentials)
+    #print(credentials)
     return credentials
 
-def create_listen_socket(port):
+def create_new_socket(port):
     s = socket.socket() # Create a socket object
     host = socket.gethostname() # Current machine name
 
@@ -22,58 +24,81 @@ def create_listen_socket(port):
     
     return s
     
-def signup():
+def new_socket_client(main_connection): # Associate a new port number and socket to each new client
+    global current_port
+    if current_port == 0: # If the current port is not initialized, we give it a random value between 2000 and 3000
+        current_port = random.randrange(2000, 3000)
+    else:
+        current_port += 1 # To give a different port number to each client
+    #print("Port : " + str(current_port))
+    
+    socket = create_new_socket(current_port)
+    main_connection.send(str.encode(str(current_port))) # Send the port number to the client
+    return socket
+    
+def signup(main_connection):
     credentials = parse_credentials_file()
 
-    received = bytes.decode(conn.recv(1024)).split(' ')
+    received = bytes.decode(main_connection.recv(1024)).split(' ')
     usr = received[0]
     pswd = received[1]
     if usr in credentials: # User already exists
-        conn.send(str.encode("0"))
+        main_connection.send(str.encode("0"))
         print("Signup unsuccessful")
         return -1
     else:
-        conn.send(str.encode("1"))
+        main_connection.send(str.encode("1"))
         print("Signup successful")
         f = open("credentials.txt", "a")
         f.write(usr + " " + pswd + "\n")
         return usr
     
-def login():
+def login(main_connection):
     credentials = parse_credentials_file()
     
-    received = bytes.decode(conn.recv(1024)).split(' ')
+    received = bytes.decode(main_connection.recv(1024)).split(' ')
     usr = received[0]
     pswd = received[1]
     if usr not in credentials:
-        conn.send(str.encode("0"))
+        main_connection.send(str.encode("0"))
         print("Login unsucessful")
-        return -1
+        return -1, -1
     elif credentials[usr] != pswd:
-        conn.send(str.encode("1"))
+        main_connection.send(str.encode("1"))
         print("Login unsucessful")
-        return -1
+        return -1, -1
     else:
-        conn.send(str.encode("2"))
+        main_connection.send(str.encode("2"))
         print("Login successful")
-        return usr
+        return usr, new_socket_client(main_connection)
+        
+def server_listener(main_connection, usr): # Listen to messages arriving from a client and displays them
+    while True: # Receive data while there is some left
+        data = bytes.decode(main_connection.recv(2048))
+        if data == "" or data == ".": # Stop communication
+            print("User " + usr + " disconnected")
+            break
+        print("From user " + usr + " : " + data)
 
-listen_socket = create_listen_socket(9999) # Port number
+main_socket = create_new_socket(10000) # Main socket, used by the server to send data and to listen to new connections. Port number is 10 000 by definition
 
 while True:
-    conn,addr = listen_socket.accept() # Wait for connection on this socket
-    print ("Connection from address " + str(addr))
-    try:
-        query = bytes.decode(conn.recv(1)) # Waiting for query
-        print(query)
-        if query == "0":
-            usr = login()
-        elif query == "1":
-            usr = signup()
-        elif query == "":
-            continue # Connection closed by the client
+    main_connection,address = main_socket.accept() # Wait for connection on this socket
+    print("Connection from address " + str(address))
+    #try:
+    query = bytes.decode(main_connection.recv(1)) # Waiting for query
+    if query == "0":
+        usr, socket_client = login(main_connection)
+        if usr == -1:
+            continue # Login unsucessful
         else:
-            raise Exception("Unexpected query")
-    finally:
-        conn.close()
-        print("Connection closed")
+            start_new_thread(server_listener, (main_connection, usr))
+    elif query == "1":
+        usr = signup(main_connection)
+    elif query == "":
+        continue # Connection closed by the client
+    else:
+        raise Exception("Unexpected query")
+    #finally:
+        #conn.close()
+        #print("Connection closed")
