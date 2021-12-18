785b55b954ab7ca15a28d7644e7da0d449c17be7
Lucas Prieels <lucas@MacBook-Pro-de-Lucas-2.local>
2021-12-18 14:02:12 +0100

    Posibility to send messages to other clients

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket, random
 from _thread import *
 
-clients = {} # List of clients connected with their username and their socket
+clients = {} # List of clients connected with their username and their connections
 current_port = 0
 
 def parse_credentials_file(): # Read the credentials file to get the users and passwords
@@ -24,7 +24,7 @@ def create_new_socket(port):
     
     return s
     
-def new_socket_client(main_connection): # Associate a new port number and socket to each new client
+def new_socket_client(client_connection): # Associate a new port number and socket to each new client
     global current_port
     if current_port == 0: # If the current port is not initialized, we give it a random value between 2000 and 3000
         current_port = random.randrange(2000, 3000)
@@ -33,68 +33,86 @@ def new_socket_client(main_connection): # Associate a new port number and socket
     #print("Port : " + str(current_port))
     
     socket = create_new_socket(current_port)
-    main_connection.send(str.encode(str(current_port))) # Send the port number to the client
+    client_connection.send(str.encode(str(current_port))) # Send the port number to the client
     return socket
     
-def signup(main_connection):
+def signup(client_connection):
     credentials = parse_credentials_file()
 
-    received = bytes.decode(main_connection.recv(1024)).split(' ')
+    received = bytes.decode(client_connection.recv(1024)).split(' ')
     usr = received[0]
     pswd = received[1]
     if usr in credentials: # User already exists
-        main_connection.send(str.encode("0"))
+        client_connection.send(str.encode("0"))
         print("Signup unsuccessful")
         return -1
     else:
-        main_connection.send(str.encode("1"))
+        client_connection.send(str.encode("1"))
         print("Signup successful")
         f = open("credentials.txt", "a")
         f.write(usr + " " + pswd + "\n")
         return usr
     
-def login(main_connection):
+def login(client_connection):
     credentials = parse_credentials_file()
     
-    received = bytes.decode(main_connection.recv(1024)).split(' ')
+    received = bytes.decode(client_connection.recv(1024)).split(' ')
     usr = received[0]
     pswd = received[1]
     if usr not in credentials:
-        main_connection.send(str.encode("0"))
+        client_connection.send(str.encode("0"))
         print("Login unsucessful")
         return -1, -1
     elif credentials[usr] != pswd:
-        main_connection.send(str.encode("1"))
+        client_connection.send(str.encode("1"))
         print("Login unsucessful")
         return -1, -1
     else:
-        main_connection.send(str.encode("2"))
+        client_connection.send(str.encode("2"))
         print("Login successful")
-        return usr, new_socket_client(main_connection)
+        return usr, new_socket_client(client_connection)
         
-def server_listener(main_connection, usr): # Listen to messages arriving from a client and displays them
-    while True: # Receive data while there is some left
-        data = bytes.decode(main_connection.recv(2048))
-        if data == "" or data == ".": # Stop communication
+def server_listener(usr): # Listen to messages arriving from a client and displays them
+    client_connection, client_listen_connection = clients[usr] # Gets the sending and listening connections for this user
+    while True:
+        recipient = bytes.decode(client_listen_connection.recv(10))
+        print(recipient)
+        if recipient == "" or recipient == ".": # Code for the client to be disconnected
             print("User " + usr + " disconnected")
-            break
-        print("From user " + usr + " : " + data)
+            clients.pop(usr) # Remove the client from the list of active users
+            break # Stop listening to this client
+        elif recipient not in clients:
+            client_listen_connection.send(str.encode("0")) # The user which need to be contacted doesn't exist or is not connected
+        else:
+            client_listen_connection.send(str.encode("1"))
+            recipient_connection, recipient_listen_connection = clients[recipient]
+            recipient_connection.send(str.encode(usr))
+            while True:
+                data = bytes.decode(client_listen_connection.recv(2048))
+                print("From user " + usr + " : " + data)
+                if data == "" or data == ".": # Stop communication with this recipient
+                    print("Change of conversation")
+                    break
+                recipient_connection.send(str.encode(data))
 
 main_socket = create_new_socket(10000) # Main socket, used by the server to send data and to listen to new connections. Port number is 10 000 by definition
 
 while True:
-    main_connection,address = main_socket.accept() # Wait for connection on this socket
+    client_connection, address = main_socket.accept() # Wait for connection on this socket
     print("Connection from address " + str(address))
     #try:
-    query = bytes.decode(main_connection.recv(1)) # Waiting for query
+    query = bytes.decode(client_connection.recv(1)) # Waiting for query
     if query == "0":
-        usr, socket_client = login(main_connection)
+        usr, socket_client = login(client_connection)
         if usr == -1:
             continue # Login unsucessful
         else:
-            start_new_thread(server_listener, (main_connection, usr))
+            client_listen_connection, address = socket_client.accept() # From now on, listen on connection client_listen_connection and send on client_connection. Address is the same as before since it's the same cliet
+            clients[usr] = (client_connection, client_listen_connection) # Add the user to the list of connected users
+            print(clients)
+            start_new_thread(server_listener, (usr,))
     elif query == "1":
-        usr = signup(main_connection)
+        usr = signup(client_connection)
     elif query == "":
         continue # Connection closed by the client
     else:
