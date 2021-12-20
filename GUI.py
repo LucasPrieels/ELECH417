7415dcd6ace50d17a95a6bdf8076811dee975011
nicoducarme <44968535+nicoducarme@users.noreply.github.com>
2021-12-20 16:11:55 +0100

    ajout interface graphique (Login et register fonctionnels)

--- GUI.py
+++ GUI.py
@@ -2,126 +2,50 @@ import tkinter as tk
 from tkinter import scrolledtext
 
 global font
-font = "Comic Sans Ms"
+font = "Arial Black"
+usr = "nico"
+user_to = "nico"
 
-
-def login():
+root = tk.Tk()
+def chat_init_gui():
     global root
     root.destroy()
     root = tk.Tk()
-
-    root.title('Login')
-    root.geometry('300x300+220+170')
+    root.title('Chat')
+    root.geometry('600x400')
     root.configure(bg='white')
     root.resizable(False, False)
 
-    log_label = tk.Label(root, text='Login', width=20, height=1, font=(font, 20, 'bold'))
-    log_label.pack()
-
-    u = tk.Label(root, text='Username :', font=(font, 14, 'bold'), bg='white')
-    u.place(x=10, y=50)
-
-    user_entry = tk.Entry(root, font=(font, 10, 'bold'), width=25, bg='powder blue')
-    user_entry.place(x=10, y=80)
-
-    p = tk.Label(root, text='Password :', font=(font, 14, 'bold'), bg='white')
-    p.place(x=10, y=110)
-
-    pass_entry = tk.Entry(root, show='*', font=(font, 10, 'bold'), width=25, bg='powder blue')
-    pass_entry.place(x=10, y=140)
-
-    resp = tk.Label(root, text='', font=(font, 10, 'bold'), bg='white')
-    resp.place(x=10, y=250)
-
-    submit =tk.Button(root, text='Submit', font=(font, 10, 'bold'), width=14, bg='green', command=lambda: print((pass_entry.get() + user_entry.get())), fg='black')
-    submit.place(x=10, y=180)
-
-    tk.Label(root, text='Don\'t Have An Account ?', bg='white', font = (font, 10, "normal")).place(x=30, y=210)
-
-    tk.Button(root, text='Register', font=(font, 10, 'underline'), bg='white', fg='blue', command = register).place(x=200, y=210)
-
-
-    root.mainloop()
-
-
-def register():
-    global root
-    root.destroy()
-    root = tk.Tk()
-
-    root.title('Register')
-    root.geometry('300x300+220+170')
-    root.configure(bg='white')
-    root.resizable(False, False)
-
-    log_label = tk.Label(root, text='Register', width=20, height=1, font=(font, 20, 'bold'))
-    log_label.pack()
-
-    u = tk.Label(root, text='Username :', font=(font, 14, 'bold'), bg='white')
-    u.place(x=10, y=50)
-
-    user_entry = tk.Entry(root, font=(font, 10, 'bold'), width=25, bg='powder blue')
-    user_entry.place(x=10, y=80)
-
-    p = tk.Label(root, text='Password :', font=(font, 14, 'bold'), bg='white')
-    p.place(x=10, y=110)
-
-    pass_entry = tk.Entry(root, show='*', font=(font, 10, 'bold'), width=25, bg='powder blue')
-    pass_entry.place(x=10, y=140)
-
-    resp = tk.Label(root, text='', font=(font, 10, 'bold'), bg='white')
-    resp.place(x=10, y=250)
-
-    submit = tk.Button(root, text='Submit', font=(font, 10, 'bold'), width=14, bg='green', command=log_func,
-                       fg='black')
-    submit.place(x=10, y=180)
-
-    tk.Label(root, text='Already Have an account ?', bg='white', font=(font, 10, "normal")).place(x=30, y=210)
-
-    tk.Button(root, text='login', font=(font, 10, 'underline'), bg='white', fg='blue',
-              command=login).place(x=200, y=210)
-
-    root.bind('<Return>', init)
-
-    root.mainloop()
-
-
-def chat():
-    root.destroy()
-    win = tk.Toplevel()
-    win.geometry('530x400')
-    win.resizable(False, False)
-    win.title(f'Chat_username')
-
-    tk.Label(win, text='Chat', bg='white', font=('arial black', 13), width=50, height=1).pack()
-
-    text = scrolledtext.ScrolledText(win, height=17, width=41, font=('arial black', 10), wrap="word")
+    tk.Label(root, text='Chat', bg='white', font=(font, 13),fg = 'black', width=50, height=1).place(x=-200, y=20)
+    text = scrolledtext.ScrolledText(root, height=17, width=41, font=(font, 10), wrap='word')
     text.place(x=10, y=40)
-    text.yview('end')
+    text.yview(tk.END)
+    text.configure(state=tk.NORMAL)
 
-    msg_entry = tk.Entry(win, font=('arial black', 13), width=25)
+    text.insert(tk.INSERT, "J'adore les gros chibre" + '\n')
+    text.insert(tk.INSERT, "J'adore les gros chibre")
+    text.configure(state=tk.DISABLED)
+
+
+
+    msg_entry = tk.Entry(root, font=(font, 13), width=25)
+
     msg_entry.place(x=10, y=365)
 
-    send = tk.Button(win, font=('arial black', 10), text='Send', bd=0, bg='blue', fg='white', width=10)
+    send = tk.Button(root, font=(font, 10), text='Send', bd=0, bg='blue', fg='black', width=10,
+                     command=lambda: print("envoyer message"))
     send.place(x=300, y=365)
 
-    tk.Label(win, font=('arial black', 13), bg='blue', fg='white', text='Users', width=12).place(y=40, x=400)
+    tk.Label(root, font=(font, 13), bg='blue', fg='black', text="send to " + user_to, width=12).place(y=40, x=400)
 
+    tk.Label(root, font=(font, 13), bg='Green', fg='black', text='Users', width=10).place(y=200, x=400)
 
-def log_func():
-    print("zeubi")
+    active_users = tk.Listbox(root, height=8, width=20)
+    active_users.place(x=400, y=230)
 
-
-def init():
-    global root
-    root = tk.Tk()
-    root.geometry('400x150')
-    root.title('login signup')
-
-    tk.Button(root, text="Login", command=login).grid(row=4, column=0)
-    tk.Button(root, text="Register", command=register).grid(row=4, column=2)
+    tk.Label(root, text='Logged In as : \n' + usr, font=(font, 10)).place(x=400, y=360)
 
     root.mainloop()
 
 
-init()
+chat_init_gui()
--- client.py
+++ client.py
@@ -5,7 +5,6 @@ from _thread import *
 import tkinter as tk
 from tkinter import scrolledtext
 font = "Arial Black"
-
 disconnection = False # It is put to True when the user wants to be disconnected
 
 
@@ -143,31 +142,37 @@ def login_gui():
 
 def chat_init_gui():
     global root
+    global usr
+    global user_to
+    user_to = usr # juste pour tester
     root.destroy()
-    root = tk.Toplevel()
-
-    root.title('Register')
+    root = tk.Tk()
+    root.title('Chat')
     root.geometry('600x400')
     root.configure(bg='white')
     root.resizable(False, False)
 
-    tk.Label(root, text='Chat', bg='white', font=(font, 13), width=50, height=1).pack()
+    tk.Label(root, text='Chat', bg='white', font=(font, 13),fg = 'black', width=50, height=1).place(x=-200, y=20)
     text = scrolledtext.ScrolledText(root, height=17, width=41, font=(font, 10), wrap='word')
     text.place(x=10, y=40)
-    text.yview('end')
+    text.yview(tk.END)
+    text.configure(state=tk.NORMAL)
 
-    text.insert('insert', "J'adore les gros chibre")
-    text.insert('end',"J'adore les gros chibre")
+    text.insert(tk.INSERT, "J'adore les gros chibre" + '\n')
+    text.insert(tk.INSERT, "J'adore les gros chibre")
+    text.configure(state=tk.DISABLED)
 
 
 
-    msg_entry = tk.Entry(root, font=(font,13),width=25)
-    msg_entry.place(x=10,y=365)
+    msg_entry = tk.Entry(root, font=(font, 13), width=25)
 
-    send = tk.Button(root, font=(font,10), text='Send',bd=0,bg='blue',fg='black',width=10,command=lambda : print("envoyer message"))
-    send.place(x=300,y=365)
+    msg_entry.place(x=10, y=365)
 
-    tk.Label(root, font=(font,13),bg='blue',fg='black',text='User',width=12).place(y=40,x=400)
+    send = tk.Button(root, font=(font, 10), text='Send', bd=0, bg='blue', fg='black', width=10,
+                     command=lambda: print("envoyer message"))
+    send.place(x=300, y=365)
+
+    tk.Label(root, font=(font, 13), bg='blue', fg='black', text="send to " + user_to, width=12).place(y=40, x=400)
 
     tk.Label(root, font=(font, 13), bg='Green', fg='black', text='Users', width=10).place(y=200, x=400)
 
@@ -177,6 +182,7 @@ def chat_init_gui():
     tk.Label(root, text='Logged In as : \n' + usr, font=(font, 10)).place(x=400, y=360)
 
     root.mainloop()
+
 
 
 def register_gui():
